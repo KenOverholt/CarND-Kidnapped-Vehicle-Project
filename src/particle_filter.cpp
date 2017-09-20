a1f3d29e4c0b4d43988168396955ec0b46686792
Ken Overholt <kroverholt@gmail.com>
2017-09-19 20:14:57 -0400

    Update particle_filter.cpp

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -63,7 +63,7 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
     double new_y;
     double new_theta;
     
-    if (fabs(yaw_rate) <= 0.1)
+    if (yaw_rate == 0)
     {
       new_x = particles[i].x + velocity*delta_t*cos(particles[i].theta);
       new_y = particles[i].y + velocity*delta_t*sin(particles[i].theta);
@@ -157,7 +157,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
         
 	      double exponent = ( pow(x_obs - mu_x,2))/(2* pow(sig_x,2)) +
                            ( pow(y_obs-mu_y,2))/(2* pow(sig_y,2)) ;
-        long double multiplier = 1/(2*M_PI*std_landmark[0]*std_landmark[1]) * exp(-exponent); //KRO finished?
+        long double multiplier = (1/(2*M_PI*std_landmark[0]*std_landmark[1])) * exp(-exponent); //KRO finished?
         if (multiplier > 0)
         {
           particles[p].weight*= multiplier;
